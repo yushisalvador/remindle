7b09e1251cadb955898dda1e88360fd431fabcc2
yccjs <yccjsalvador@gmail.com>
2022-07-09 17:27:25 +0900

    feat: edit settings screen, add modal for contact us

--- components/SettingsComponent.js
+++ components/SettingsComponent.js
@@ -5,21 +5,50 @@ import {
   View,
   Text,
   StyleSheet,
+  Pressable,
 } from "react-native";
+import AppModal from "./Modals/AppModal";
 
-export default function SettingsComponent({ menuData }) {
+export default function SettingsComponent({
+  menuData,
+  modalVisible,
+  setModalVisible,
+}) {
   return (
-    <ScrollView style={styles.container}>
-      {menuData.map(({ title, definition, index }) => (
-        <TouchableOpacity key={title}>
-          <View style={styles.singleMenu}>
-            <Text style={styles.mainText}>{title}</Text>
-            <Text style={styles.subText}>{definition}</Text>
+    <>
+      <AppModal
+        modalVisible={modalVisible}
+        setModalVisible={setModalVisible}
+        title="Contact Us"
+        modalBody={
+          <View>
+            <Text style={{ fontSize: 18 }}>
+              For account deletion, bug reports and feedbacks, please email us
+              at <b>aremindly.gmail.com</b>
+            </Text>
           </View>
-          <View style={styles.divider} />
-        </TouchableOpacity>
-      ))}
-    </ScrollView>
+        }
+      />
+      <ScrollView style={styles.container}>
+        {menuData.map(({ title, definition, index, onPress }) => (
+          <TouchableOpacity key={title} onPress={onPress}>
+            <View style={styles.singleMenu}>
+              <Text style={styles.mainText}>{title}</Text>
+              <Text style={styles.subText}>{definition}</Text>
+            </View>
+            <View style={styles.divider} />
+          </TouchableOpacity>
+        ))}
+        {/* <View>
+          {modalVisible && (
+            <ContactUsModal
+              setModalVisible={setModalVisible}
+              modalVisible={modalVisible}
+            />
+          )}
+        </View> */}
+      </ScrollView>
+    </>
   );
 }
 
--- navigation/screens/SettingsScreen.js
+++ navigation/screens/SettingsScreen.js
@@ -1,35 +1,35 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { StyleSheet } from "react-native";
 import SettingsComponent from "../../components/SettingsComponent";
 
 export default function SettingsScreen({ navigation }) {
+  const [modalVisible, setModalVisible] = useState(false);
+
   const menuData = [
     {
       title: "About",
       definition: "About the App, About the team",
+      onPress: () => {},
     },
     {
       title: "Feedback & Help",
       definition: "Contact us",
+      onPress: () => {
+        setModalVisible(true);
+      },
     },
     {
       title: "Screen",
       definition: "Enable Dark Mode!",
+      onPress: () => {},
     },
   ];
 
-  return <SettingsComponent menuData={menuData} />;
+  return (
+    <SettingsComponent
+      menuData={menuData}
+      modalVisible={modalVisible}
+      setModalVisible={setModalVisible}
+    />
+  );
 }
-
-const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  // },
-  // list: {
-  //   flex: 1,
-  //   width: "100%",
-  //   marginTop: 24,
-  // },
-});
